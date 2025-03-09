858586235a24dfd69578cd1e9d73de9366565f49
Kapil-IN <kapilsharma.learning@gmail.com>
2025-03-09 22:16:46 +0530

    Add files via upload

---
+++ assignment2_task1.py
@@ -0,0 +1,11 @@
+#Assignment 2:
+## Module 3: Control Structures in Python
+###Task 1: Check if a Number is Even or Odd
+####Author: Kapil Sharma
+
+num = int(input('Enter a number: '))
+
+if num%2 == 0:
+    print(num," is an even number.")
+else:
+    print(num," is an odd number.")
---
+++ assignment2_task2.py
@@ -0,0 +1,10 @@
+#Assignment 2:
+## Module 3: Control Structures in Python
+###Task 2: Sum of Integers from 1 to 50 using a Loop
+####Author: Kapil Sharma
+
+sum = 0
+for i in range(1,51):
+    sum+=i
+print("The sum of the numbers from 1 to 50 is: ", sum)
+
